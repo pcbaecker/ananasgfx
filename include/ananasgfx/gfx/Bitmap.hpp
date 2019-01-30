c79882b59a21a398ddf74972b5329216f02a3190
Philipp C. Baecker <baecker@orderheaven.com>
2019-01-30 16:52:40 +0100

    gfx::Bitmap functionality extended

--- include/ananasgfx/gfx/Bitmap.hpp
+++ include/ananasgfx/gfx/Bitmap.hpp
@@ -19,7 +19,21 @@ namespace gfx {
         size_t getHeight() const noexcept;
         uint8_t getChannels() const noexcept;
 
+        void setPixel(uint16_t x, uint16_t y, uint8_t r) noexcept;
+        uint8_t getPixelGrayscale(uint16_t x, uint16_t y) const noexcept;
+
+        void setPixel(uint16_t x, uint16_t y, uint8_t r, uint8_t g) noexcept;
+        std::tuple<uint8_t,uint8_t> getPixelGrayAlpha(uint16_t x, uint16_t y) const noexcept;
+
+        void setPixel(uint16_t x, uint16_t y, uint8_t r, uint8_t g, uint8_t b) noexcept;
+        std::tuple<uint8_t,uint8_t,uint8_t> getPixelRgb(uint16_t x, uint16_t y) const noexcept;
+
+        void setPixel(uint16_t x, uint16_t y, uint8_t r, uint8_t g, uint8_t b, uint8_t a) noexcept;
+        std::tuple<uint8_t,uint8_t,uint8_t,uint8_t> getPixelRgba(uint16_t x, uint16_t y) const noexcept;
+
+        std::optional<std::shared_ptr<Bitmap>> resize(size_t width, size_t height) const noexcept;
         bool saveAsFile(const std::string& filename) const noexcept;
+        std::shared_ptr<Bitmap> asGrayscale() const noexcept;
 
     private:
         void* pData = nullptr;
