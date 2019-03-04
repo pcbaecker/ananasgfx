e9c4d80992daa8e1c4eecc56936364f533ae63b2
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-03-04 13:51:12 +0000

    Deploy pcbaecker/ananasgfx to github.com/pcbaecker/ananasgfx.git:gh-pages

--- examples/AnanasGfxExamples.js
+++ examples/AnanasGfxExamples.js
@@ -194,7 +194,7 @@ Module['FS_createPath']('/', 'resource', true, true);
   }
 
  }
- loadPackage({"files": [{"start": 0, "audio": 0, "end": 19234, "filename": "/resource/compare.png"}, {"start": 19234, "audio": 0, "end": 35411, "filename": "/resource/icon_circle.png"}, {"start": 35411, "audio": 0, "end": 50105, "filename": "/resource/icon_arrowdropdown.png"}, {"start": 50105, "audio": 0, "end": 177849, "filename": "/resource/Roboto-Bold.ttf"}, {"start": 177849, "audio": 0, "end": 305337, "filename": "/resource/Roboto-Medium.ttf"}, {"start": 305337, "audio": 0, "end": 307152, "filename": "/resource/icon_lightbulb.png"}], "remote_package_size": 307152, "package_uuid": "122561f1-7e71-4ebd-aeda-9b8e37d8d7bb"});
+ loadPackage({"files": [{"start": 0, "audio": 0, "end": 19234, "filename": "/resource/compare.png"}, {"start": 19234, "audio": 0, "end": 35411, "filename": "/resource/icon_circle.png"}, {"start": 35411, "audio": 0, "end": 50105, "filename": "/resource/icon_arrowdropdown.png"}, {"start": 50105, "audio": 0, "end": 177849, "filename": "/resource/Roboto-Bold.ttf"}, {"start": 177849, "audio": 0, "end": 305337, "filename": "/resource/Roboto-Medium.ttf"}, {"start": 305337, "audio": 0, "end": 307152, "filename": "/resource/icon_lightbulb.png"}], "remote_package_size": 307152, "package_uuid": "b0790e20-e726-4822-92a5-0443e659f01a"});
 
 })();
 
